039ad6bf2bbd7cb66ac709b5b05b69aec5863f87
dome-sieb <sieberdominique10@gmail.com>
2024-05-28 18:01:12 +0200

    Update main_screen.dart

--- lib/src/features/main_screen.dart
+++ lib/src/features/main_screen.dart
@@ -8,63 +8,58 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  String? zip;
-  late Future<String?> getCityFromZipFuture;
-
-  @override
-  void initState() {
-    super.initState();
-    getCityFromZip(zip!);
-  }
+  String? getCityFromZip;
+  late Future<String?> getCityfromZipFuture;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-          child: Column(children: [
-            const TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), labelText: "Postleitzahl"),
-            ),
-            const SizedBox(height: 32),
-            OutlinedButton(
-              onPressed: () async {
-                getCityFromZip = await getCityFromZipFuture;
-                FutureBuilder(
-                    future: getCityFromZip("10115"),
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData &&
-                          snapshot.connectionState == ConnectionState.done) {
-                        return const Text(
-                            "Die Stadt zu Postleitzahl ist §{snapshot.data}");
-                      } else if (snapshot.connectionState !=
-                          ConnectionState.done) {
-                        return const CircularProgressIndicator();
-                      } else {
-                        return const Icon(Icons.error);
-                      }
-                    });
-              },
-              child: const Text("Suche"),
-            ),
-            const SizedBox(height: 32),
-            Text("Ergebnis: Noch keine PLZ gesucht",
-                style: Theme.of(context).textTheme.labelLarge),
-          ]),
-        ),
-      ),
-    );
+        body: Center(
+            child: Padding(
+                padding: const EdgeInsets.all(60.0),
+                child: Column(children: [
+                  const TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Postleitzahl"),
+                  ),
+                  const SizedBox(height: 32),
+                  OutlinedButton(
+                    onPressed: () async {
+                      getCityFromZip = await getCityfromZipFuture;
+                      FutureBuilder(
+                          future: getCity(),
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData &&
+                                snapshot.connectionState ==
+                                    ConnectionState.done) {
+                              return const Text(
+                                  "Die Stadt zur Postleityahl ist: §{snapshot.data}");
+                            } else if (snapshot.connectionState !=
+                                ConnectionState.done) {
+                              return const CircularProgressIndicator();
+                            } else {
+                              return const Icon(Icons.error);
+                            }
+                          });
+                    },
+                    child: const Text("Suche"),
+                  ),
+                  const SizedBox(height: 32),
+                  Text(
+                    "Ergebnis: Noch keine PLZ gesucht",
+                    style: Theme.of(context).textTheme.labelLarge,
+                  )
+                ]))));
   }
 
   @override
   void dispose() {
     super.dispose();
+    getCityfromZipFuture = getCityFromZip! as Future<String?>;
   }
 
-  Future<String> getCityFromZip(String zip) async {
-    // simuliere Dauer der Datenbank-Anfrage
+  Future<String> getCityFromZipFuture(String zip) async {
     await Future.delayed(const Duration(seconds: 3));
 
     switch (zip) {
@@ -73,9 +68,9 @@ class _MainScreenState extends State<MainScreen> {
       case "20095":
         return 'Hamburg';
       case "80331":
-        return 'München';
+        return 'Muenchen';
       case "50667":
-        return 'Köln';
+        return 'Koeln';
       case "60313":
         return 'Frankfurt am Main';
       default:
@@ -83,3 +78,5 @@ class _MainScreenState extends State<MainScreen> {
     }
   }
 }
+
+getCity() {}
